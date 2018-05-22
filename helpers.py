b30dee7be205e67e10ed0b5adf46d1f936e4804a
eemoir <33586203+eemoir@users.noreply.github.com>
2018-05-22 14:10:24 -0400

    Added files via upload

---
+++ helpers.py
@@ -0,0 +1,37 @@
+from nltk.tokenize import sent_tokenize
+
+def lines(a, b):
+    """Return lines in both a and b"""
+    q = set(a.splitlines())
+    r = set(b.splitlines())
+
+    return list(q & r)
+
+
+def sentences(a, b):
+    """Return sentences in both a and b"""
+
+    q = set(sent_tokenize(a))
+    r = set(sent_tokenize(b))
+
+    return list(q & r)
+
+
+def substrings(a, b, n):
+    """Return substrings of length n in both a and b"""
+    q = set(substringHelper(a, n))
+    r = set(substringHelper(b, n))
+
+    return list(q & r)
+
+def substringHelper(a, n):
+    l = []
+    x = 0
+    while x<n:
+        y = x
+        while y < (len(a)+1-n):
+            l.append(a[y:y+n])
+            y+=n
+        x+=1
+    return l
+
